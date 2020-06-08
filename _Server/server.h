862c709bd92c28f3a937f28ef5a1f690c8b66a49
IlKaiser <samuele.rinaldi90@gmail.com>
2020-06-08 19:34:46 +0200

    Ip experiments

--- _Server/server.h
+++ _Server/server.h
@@ -6,7 +6,7 @@
 /* Configuration parameters */
 #define DEBUG           1   // display debug messages
 #define MAX_CONN_QUEUE  3   // max number of connections the server can queue
-#define SERVER_ADDRESS  "127.0.0.1"
+#define SERVER_ADDRESS  "21.255.185.235" //AWS
 #define SERVER_COMMAND  "QUIT\n"
 #define MAX_SIZE        128
 #define SERVER_PORT     2015
