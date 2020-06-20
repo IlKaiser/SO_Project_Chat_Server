4597203feeb0c82d8e62cc1209b6bc4271f9e4df
gartic99 <artic.1804049@studenti.uniroma1.it>
2020-06-20 20:46:49 +0200

    header for list request

--- _Server/server.h
+++ _Server/server.h
@@ -11,11 +11,14 @@
 #define MAX_SIZE        128
 #define SERVER_PORT     2015
 #define ERROR_MSG       "0xAFFAF\n"
+#define DISCONNECTED    0xAFFAF
 #define ALONE_MSG       "Alone\n"
 #define OK_MSG          "OK\n"
+#define LIST_COMMAND    "_LIST_\n"
 
 #define SEM_NAME        "/server"
-#define AWS             1
+#define AWS             0
+
 //Struct define
 typedef struct handler_args_s
 {
@@ -31,10 +34,22 @@ void *thread_connection_handler(void *arg);
 void connection_handler(int socket_desc, struct sockaddr_in* client_addr);
 
 // function that formats the user list that server sends
-void list_formatter(char buf[]);
+void list_formatter(char buf[],int socket_desc);
 
 //function that sends message to reciver
 void send_mess(int index,char buff[]);
 
 //close db connection
-static void exit_nicely(PGconn *conn, PGresult   *res);
+static void exit_nicely(PGconn *conn, PGresult   *res,int socket_desc);
+
+//disconneciotn handler
+void disconnection_handler(int index);
+
+// next postion setter
+void set_next_position();
+
+// set disconnect into socket array
+void set_disconnected(int socket_desc);
+
+//check credentials in db
+int login(char* credentials,int socket_desc);
