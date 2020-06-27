d3e9f25c3888b8db92bfe57e6baac90b139541dc
IlKaiser <samuele.rinaldi90@gmail.com>
2020-06-27 19:23:57 +0200

    Other fixes

--- Common/common.c
+++ Common/common.c
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
 #include <sys/socket.h>
 
 #include "common.h"
--- Common/common.h
+++ Common/common.h
@@ -1,3 +1,4 @@
+#pragma once
 // macros for handling errors
 #define handle_error_en(en, msg)    do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
 #define handle_error(msg)           do { perror(msg); exit(EXIT_FAILURE); } while (0)
