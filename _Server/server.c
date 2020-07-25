d30050a0fe459021d5e5413a31c1334ed5395934
IlKaiser <samuele.rinaldi90@gmail.com>
2020-07-25 17:27:59 +0200

    Fix typo

--- _Server/server.c
+++ _Server/server.c
@@ -343,10 +343,10 @@ void connection_handler(int socket_desc, struct sockaddr_in* client_addr) {
             printf ("searching messages between %s %s",user_name,target_user_name);
             const char* paramValue[2] = {trim_username,trim_to};
             res = PQexecParams(conn,"select mess._fro,mess.co,data from(" 
-                                    "select m._from as _fro, m.mes as co, m.data as data, m._time as _time"
+                                    "select m._from as _fro, m.mes as co, m.data as data, m._time as _time "
                                     "from messaggi as m where m._from=$1 and m._to=$2" 
                                     "union all" 
-                                    "select m1._from as _fro, m1.mes as co, m1.data as data, m1._time as _time"
+                                    "select m1._from as _fro, m1.mes as co, m1.data as data, m1._time as _time "
                                     "from messaggi as m1" 
                                     "where m1._from=$2 and m1._to=$1)"
                                     "as mess order by mess._time desc limit 10",
