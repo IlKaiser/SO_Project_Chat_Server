e2f93c2013064b194d4d8589fa3dd8b703fe5a3b
IlKaiser <samuele.rinaldi90@gmail.com>
2020-07-30 12:58:14 +0200

    Revert "Array defrag"
    This reverts commit 21ab3acc5e1d4edae1fadc25ef0974491428383c.

--- _Server/server.c
+++ _Server/server.c
@@ -617,7 +617,6 @@ void disconnection_handler(int index){
         if(ret){handle_error("Disconnection error");}
         set_disconnected(index);
         set_next_position();
-        array_defrag();
     }
     ret=sem_wait(sem);
     //Clear all things we used
@@ -719,18 +718,4 @@ int login(char* credentials,int socket_desc){
     }
     int ris=PQntuples(res);
     return ris;
-}
-void array_defrag(){
-    int i,j;
-    for(i=0;i<previous_size;i++){
-        if(sockets[i]==DISCONNECTED){
-            for(j=i;j<previous_size;j++){
-                if(sockets[j]!=DISCONNECTED){
-                    sockets[i]=sockets[j];
-                    break;
-                }
-            }
-        }
-    }
-    set_next_position();
 }
